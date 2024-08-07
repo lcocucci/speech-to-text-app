6514f3bc124f233000e168d0e28fa387bfd148da
lcocucci <leococucci@gmail.com>
2024-08-07 10:51:25 -0300

    Implementación funcional de whisper mediante archivo de audio recibido desde el cliente (por micrófono)

--- app/src/main.py
+++ app/src/main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from .apis.stt_api import router
+from datetime import datetime
+from .services.whisper_audiofile_service import stt_whisper_audiofile
     
 app = FastAPI()
 app.include_router(router, prefix='/api', tags=['stt'])
 
+# Renderizamos el archivo index.html en la raíz de la aplicación cliente
 @app.get('/')
 def root():
     with open('app/src/index.html', 'r') as file:
         html_response = file.read()
     return HTMLResponse(content = html_response)
+
+# Realizamos la conexión a websockets
+@app.websocket('/ws')
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept() # Aceptamos la conexión de websockets
+    while True:
+        data = await ws.receive_bytes() # Quedamos a la espera de recibir datos del cliente
+        now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        file_path = f'app/src/samples/audio/stt-staf-{now}.wav' # Guardamos la ubicación exacta del archivo
+        with open(file_path, 'wb') as f:
+            f.write(data)
+        print('Archivo de audio recibido y guardado')
+        await ws.send_text('Archivo de audio guardado')
+        stt_whisper_audiofile(file_path)
+        print('Transcripción Completada :)')
+        
--- app/src/services/whisper_audiofile_service.py
+++ app/src/services/whisper_audiofile_service.py
@@ -1,13 +1,12 @@
 import whisper
 from datetime import datetime
 
-now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-
 # Desde un archivo de audio
 def stt_whisper_audiofile(audiofile: str):
     model = whisper.load_model('base')
     transcription = model.transcribe(audiofile)
 
+    now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
     with open(f'app\\src\\samples\\transcriptions\\stt-wl-{now}.txt', 'w') as f:
         f.write(transcription['text'])
         
